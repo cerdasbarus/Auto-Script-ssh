525ecf7ca67abf9a518e6404b8c43da286e7078e
cerdascharo <99654108+cerdasbarus@users.noreply.github.com>
2023-06-14 19:20:37 +0700

    Add files via upload

---
+++ ins-dropbear.sh
@@ -0,0 +1,10 @@
+apt -y remove dropbear
+sleep 0.5
+apt -y purge dropbear
+sleep 0.5
+apt-get -y --purge remove dropbear
+sleep 0.5
+apt -y install dropbear
+wget -O /etc/default/dropbear https://raw.githubusercontent.com/Vlukss/AutoScriptXray-ssh/main/dropbear
+sleep 0.5
+/etc/init.d/dropbear restart
